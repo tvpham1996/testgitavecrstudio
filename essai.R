ecb0d83e6bc6b1466dc5c7a096cec4a84d67bf01
Tuong Vy Pham <tv.pham1996@gmail.com>
2020-09-18 14:44:06 +0200

    Ajout de ligne ...

--- essai.R
+++ essai.R
@@ -2,4 +2,7 @@
 # Comment ça va, les amis ?
 # Je veux manger une raclette
 # cat essai.R sert à lire le fichier
-# pull ?
+# pull ? Sert à récupérer les modifications depuis le dépôt distant
+
+#Reste de la divison euclidienne
+67%%12
